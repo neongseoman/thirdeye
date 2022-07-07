e81dd7bd40a50e67a0f0219215824338bc917362
baggeunsu_mac <rmstn8426@gmail.com>
2022-07-07 17:03:49 +0900

    package.json engines 추가

--- src/app.js
+++ src/app.js
@@ -3,12 +3,10 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var indexRouter = require('./routes/index.js');
+var usersRouter = require('./routes/users.js');
 
 var app = express();
-app.set('view engine', 'ejs'); //'ejs'탬플릿을 엔진으로 한다.
-app.set('views', path.join(__dirname, 'public/views')); //폴더, 폴더경로 지정
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -18,4 +16,8 @@ app.use(express.static(path.join(__dirname, 'src/public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+app.set('view engine', 'ejs'); //'ejs'탬플릿을 엔진으로 한다.
+app.set('views', path.join(__dirname, 'public/views')); //폴더, 폴더경로 지정
+
 module.exports = app;
